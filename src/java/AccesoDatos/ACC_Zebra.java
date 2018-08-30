ca1d3c3d2360cb10e72fee34002bb025ddb8d8e5
music <music@Antonio-Lopez>
2018-08-30 11:39:49 -0500

    Merge origin/master into HEAD
    Conflicts:
    	src/java/AccesoDatos/EnvioDatosEtiqueta.java

--- src/java/AccesoDatos/ACC_Zebra.java
+++ src/java/AccesoDatos/ACC_Zebra.java
@@ -34,6 +34,7 @@ public class ACC_Zebra {
         return Instance;
     }
     String txt = "";
+
     public void PrintTargetPT(Zebra_noti_PT z) throws IOException {
         String texto;
         if (z.getDescripcion().length() < 74) {
@@ -117,12 +118,12 @@ public class ACC_Zebra {
 
 //        PrintService ps = PrintServiceLookup.lookupDefaultPrintService();
         PrintService ps = impresoras(impresora(z.getPuesto_trabajo()));
-        txt += "-" +impresora(z.getPuesto_trabajo());
+        txt += "-" + impresora(z.getPuesto_trabajo());
         try (FileWriter writer = new FileWriter("C:\\Users\\Portal Sap\\Desktop\\foo.txt")) {
-                    writer.write(txt);
-                    writer.flush();
-                    // do more stuff with writer
-                }
+            writer.write(txt);
+            writer.flush();
+            // do more stuff with writer
+        }
         DocFlavor flavor = DocFlavor.BYTE_ARRAY.AUTOSENSE;
         DocPrintJob dj = ps.createPrintJob();
         Doc doc = new SimpleDoc(texto.getBytes(), flavor, null);
@@ -140,7 +141,7 @@ public class ACC_Zebra {
         for (PrintService impresora : printServices) { //Recorremos el array de servicios de impresion
             System.out.println("Printer: " + impresora.getName());
             txt += "\nPrinter: " + impresora.getName();
-                
+
             if (impresora.getName().contentEquals(nombre)) { // Si el nombre del servicio es el mismo que el que buscamos
                 return impresora; // Nos devuelve el servicio 
             }
@@ -160,7 +161,8 @@ public class ACC_Zebra {
             ps.setString(1, orden);
             rs = ps.executeQuery();
             while (rs.next()) {
-                zb.setCliente(rs.getString("nombre"));
+                zb.setRuta(rs.getString("ruta"));
+                zb.setStock(rs.getString("stock"));
             }
         } catch (Exception e) {
             System.err.println("Error: " + e);
@@ -192,8 +194,8 @@ public class ACC_Zebra {
         return zb;
     }
 
-    public Zebra_noti_PT DatosFaltantesPosiciones(String orden, String operacion) {
-        Zebra_noti_PT zb = new Zebra_noti_PT();
+    public String DatosFaltantesPosiciones(String orden, String operacion) {
+        String pto = "";
         Conexion con = new Conexion();
         Connection conn = con.ObtenerConexion();
         ResultSet rs;
@@ -205,20 +207,20 @@ public class ACC_Zebra {
             ps.setString(2, operacion);
             rs = ps.executeQuery();
             while (rs.next()) {
-                zb.setPuesto_trabajo(rs.getString("puesto_trabajo"));
+                pto = rs.getString("puesto_trabajo");
             }
         } catch (Exception e) {
             System.err.println("Error: " + e);
         }
         con.CerrarConexion(conn);
 
-        return zb;
+        return pto;
     }
 
     public void guardaEtiquetaDB(Zebra_noti_PT zl) {
         Conexion cnx = new Conexion();
         Connection con = cnx.ObtenerConexion();
-        String query = "{CALL PP.guarda_etiquetaPT(?,?,?,?,?,?,?,?,?,?,?,?,?,?)}";
+        String query = "{CALL PP.guarda_etiquetaPT(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}";
         try {
             PreparedStatement ps = con.prepareStatement(query);
             ps.setString(1, zl.getOrden());
@@ -235,12 +237,38 @@ public class ACC_Zebra {
             ps.setString(12, zl.getDescripcion());
             ps.setString(13, "");
             ps.setString(14, zl.getNro_material());
+            ps.setString(15, zl.getRuta());
+            ps.setString(16, zl.getStock());
+            ps.setString(17, zl.getUsuario());
             ps.executeUpdate();
         } catch (Exception e) {
-            System.err.println("Error en ValidarUMedida ACC_UnidadesMedida por: " + e);
+            System.err.println("Error por: " + e);
         } finally {
             cnx.CerrarConexion(con);
         }
+    }
+
+    public String[] getImp(String pto) {
+        String[] prop = new String[2];
+        Conexion cnx = new Conexion();
+        Connection con = cnx.ObtenerConexion();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sql = "{call cnf.GetDirImp(?)}";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, pto);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                prop[0] = rs.getString("dir_ip");
+                prop[1] = rs.getString("tipo");
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        } finally {
+            cnx.CerrarConexion(con);
+        }
+        return prop;
     }
 
     public static void main(String[] args) throws IOException {
@@ -258,4 +286,78 @@ public class ACC_Zebra {
 //        ACC_Zebra.ObtenerInstancia().PrintTargetPT(zb);
 
     }
+
+    public String ConvertCodeZebra(Zebra_noti_PT z) {
+        String Concatedes = "";
+        if (z.getDescripcion().length() < 70) {
+            Concatedes = "^FT799,253^A0I,21,21^FB793,1,0^FH\\^FD^FS\n"
+                    + "^FT799,285^A0I,21,21^FB792,1,0^FH\\^FD" + z.getDescripcion().substring(0, z.getDescripcion().length()).replace("°", "_C2_B0") + "^FS\n";
+        } else {
+            Concatedes = "^FT799,253^A0I,21,21^FB793,1,0^FH\\^FD" + z.getDescripcion().substring(69, z.getDescripcion().length()).replace("°", "_C2_B0") + "^FS\n"
+                    + "^FT799,285^A0I,21,21^FB792,1,0^FH\\^FD" + z.getDescripcion().substring(0, 69).replace("°", "_C2_B0") + "^FS\n";
+        }
+        return "^XA\n"
+                + "^MMT\n"
+                + "^PW807\n"
+                + "^LL0408\n"
+                + "^LS0\n"
+                + "^FO704,320^GFA,01152,01152,00012,:Z64:\n"
+                + "eJzVk7Fr20AUxt9ZISoUZAcKCU2wMoYMJqPAIaf/ImND"
+                + "xg6poUshhVNqSIeWLFkLMXQMmIyGM9ih/0C2rJdOpi0Xe"
+                + "ygIJPT6TndSl5QUOvXJoJ8/7t777jsE8A/Vood+sXk9UD"
+                + "GLzatRC89gCf6wtq69bsHuvbE/mQK0ifH8Yl/MAYKtAPS7"
+                + "Mz1TAP64zfDLBaakN0fP+eXnD+2U9vKsQHz9/pMg7mxrrQ9"
+                + "PP7YTgN0uLlD0xz7tjaL1u+/r/REQ70bhAsN++ob6dL5pPQx"
+                + "O1CvSw6wQ6J/MOXloZgW/9xIVJn+fCx/UGN7itDpxB7RyyKYgc"
+                + "sc+wFPpeIP+4tRyRNFVG3pMeTh3W9mAybxixTAvB1DMCrQu9WUQC"
+                + "gQ6xgEIO2CDodHLAWsNpeBIlxx510Yvh+0YPs4cL1QL7eBNGJr+5dF"
+                + "i6sOcHgNXnpxcWV4h/1Y3jQOtZxULxLTibCIrXsHfOi2v1/Oc1Xp3BHJ"
+                + "k+wM1F24uxXlwVPo8AIbznnA+PXnFnX9Yxdy3frYozR8u0DXjMrE5PAFfTl"
+                + "Q3MbxE+s+ecLk19GzzZcke6TkUNvPEk2OWVvkjrtr8ycRwaOMx90jHdfcYwLGUiWUfONproVKBPa2pt86lqeZXdMvpY3lx03rki/mv6xduwusD:C017\n"
+                + "^FT704,322^A0I,28,28^FB108,1,0^FH\\^FDPto Tbjo:^FS\n"
+                + "^FT424,368^A0I,27,26^FB79,1,0^FH\\^FDFECHA:^FS\n"
+                + "^FT334,369^A0I,27,26^FB120,1,0^FH\\^FD " + z.getFecha() + "^FS\n"
+                + //// Fecha
+                "^FT198,370^A0I,27,26^FB70,1,0^FH\\^FDHORA:^FS\n"
+                + "^FT114,370^A0I,27,26^FB94,1,0^FH\\^FD" + z.getHora() + "^FS\n"
+                + "^FT707,28^A0I,23,24^FB700,1,0^FH\\^FD" + z.getCliente() + "^FS\n"
+                + "^BY2,3,99^FT744,139^BCI,,Y,N\n"
+                + "^FD>;" + z.getNro_material() + ">6-" + z.getLote() + "->5123456>" + z.getCantidad() + "^FS\n"
+                + "^FT423,328^A0I,27,26^FB121,1,0^FH\\^FDCANTIDAD:^FS\n"
+                + "^FT291,328^A0I,27,26^FB126,1,0^FH\\^FD" + z.getCantidad() + "^FS\n"
+                + "^FT797,75^A0I,28,28^FB94,1,0^FH\\^FDORDEN:^FS\n"
+                + "^FT692,75^A0I,28,28^FB141,1,0^FH\\^FD" + z.getOrden() + "^FS\n"
+                + "^FT515,75^A0I,28,28^FB70,1,0^FH\\^FDLOTE:^FS\n"
+                + "^FT429,75^A0I,28,28^FB157,1,0^FH\\^FD" + z.getLote() + "^FS\n"
+                + "^FT209,75^A0I,28,28^FB25,1,0^FH\\^FDA:^FS\n"
+                + "^FT174,75^A0I,28,28^FB135,1,0^FH\\^FD" + z.getAncho() + "^FS\n"
+                + "^FO462,316^GB126,0,7^FS\n"
+                + "^FO36,65^GB139,0,5^FS\n"
+                + "^FO272,64^GB157,0,5^FS\n"
+                + "^FO552,64^GB141,0,5^FS\n"
+                + "^FO54,317^GB99,0,4^FS\n"
+                + "^FO166,318^GB125,0,5^FS\n"
+                + "^FO15,359^GB101,0,6^FS\n"
+                + "^FO213,358^GB120,0,6^FS\n"
+                + "^FT588,367^A0I,28,28^FB145,1,0^FH\\^FD" + z.getRuta() + "^FS\n"
+                + "^FT703,367^A0I,28,28^FB96,1,0^FH\\^FD" + z.getStock() + "^FS\n"
+                + "^FT583,324^A0I,28,28^FB120,1,0^FH\\^FD" + z.getPuesto_trabajo() + "^FS\n"
+                + "^FT150,327^A0I,27,26^FB91,1,0^FH\\^FD" + z.getUm() + "^FS\n"
+                + "^FT797,28^A0I,21,21^FB81,1,0^FH\\^FDCLIENTE:^FS\n"
+                + Concatedes
+                + "^XZ";
+    }
+    public String ConvertCodeZebraTLP(Zebra_noti_PT z) {
+        String Concatedes = "";
+        if (z.getDescripcion().length() < 70) {
+            Concatedes = "^FT799,253^A0I,21,21^FB793,1,0^FH\\^FD^FS\n"
+                    + "^FT799,285^A0I,21,21^FB792,1,0^FH\\^FD" + z.getDescripcion().substring(0, z.getDescripcion().length()).replace("°", "_C2_B0") + "^FS\n";
+        } else {
+            Concatedes = "^FT799,253^A0I,21,21^FB793,1,0^FH\\^FD" + z.getDescripcion().substring(69, z.getDescripcion().length()).replace("°", "_C2_B0") + "^FS\n"
+                    + "^FT799,285^A0I,21,21^FB792,1,0^FH\\^FD" + z.getDescripcion().substring(0, 69).replace("°", "_C2_B0") + "^FS\n";
+        }
+        return "N\n"
+               +"A677,362,2,4,1,1,N,\"LEON01\"";
+    }
+    
 }
